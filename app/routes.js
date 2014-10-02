d79e80ccea60e79d500d5a83b98bb1fcdfc69cba
Michael Malet <michael@nervd.com>
2014-10-02 23:29:40 +0100

    Disable HTML5 mode as it prevents us being able to reload the plugin

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,5 @@
 (function(){
   app.config(function($routeProvider, $locationProvider){
-    $locationProvider.html5Mode(true);
     $routeProvider.when('/', {
       controller: 'HomeCtrl',
       templateUrl: '/app/views/index.html'
